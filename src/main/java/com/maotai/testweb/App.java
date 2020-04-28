08989c223e652d2b0f2bf1a06405af555badbd2a
node-151 <node@qq.com>
2020-04-29 01:40:19 +0800

    jenkins-demo

--- src/main/java/com/maotai/testweb/App.java
+++ src/main/java/com/maotai/testweb/App.java
@@ -1,13 +1,21 @@
 package com.maotai.testweb;
 
 /**
- * Hello world!
- *
- */
+ *  * Hello world!
+ *   *
+ *    */
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! i am maotai" );
+
+
+ public static void main (String[] args) throws InterruptedException {
+        System.out.println("Jenkins-demo");
+        for (int i=1000;i>0;i--){
+
+            Thread.sleep(1000);
+            System.out.println("运行倒计时"+i);
+        }
+        System.out.println("demo is over");
+
     }
 }
